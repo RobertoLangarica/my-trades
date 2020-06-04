6b78fbc68a0a2da02d922a0c14d90eefb17cd5aa
Roberto <roberto@villavanilla.com>
2020-06-03 19:11:03 -0500

    no message

---
+++ frontend/build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+wdir=$(pwd)
+mkdir temp
+tmpDir=$? 
+imageName=""
+if [ "$tmpDir" == 0 ]; then
+    cd temp
+    git clone --single-branch --branch release https://github.com/RobertoLangarica/my-trades.git
+    clone=$?
+    if [ "$clone" == 0 ]; then
+        cd my-trades/frontend
+        version=$(grep -w -i '"version"' package.json | cut -d '"' -f 4)
+        docker build -t $imageName:r-$version -t $imageName .
+        built=$?
+        if [ "$built" == 0 ]; then
+            docker push $imageName:r-$version
+            docker push $imageName:latest
+        fi
+    fi
+    cd $wdir
+    # git removes the write access to some files
+    chmod -R +w temp
+    rm -r temp
+
+    if [ "$built" == 0 ]; then
+        echo 'Build v-'$version' COMPLETED'
+    else
+        echo 'A problem ocurred while creating the build image'
+    fi
+
+else 
+echo 'Unable to create directory temp'
+fi 
